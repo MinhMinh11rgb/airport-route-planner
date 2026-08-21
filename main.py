2c18e48b502f5af60965583fa9a8975e96cb5009
White3011 <s4112692@rmit.edu.vn>
2026-08-21 17:22:28 +0700

    Finished distance calculations using harvensine distance.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import math
 airportPath = "data/airports.dat"
 routePath = "data/routes.dat"
 def load_airports(path):
@@ -30,6 +31,35 @@ def load_routes(path, airports):
     return graph
 
 
+def harvensine_distance(lat1,lon1,lat2,lon2):
+    r = 6371 #earth radius in km
+    lat1Rad = math.radians(lat1)     
+    lat2Rad = math.radians(lat2)
+    lon1Rad = math.radians(lon1)
+    lon2Rad = math.radians(lon2)
+
+    delta_lat = lat2Rad - lat1Rad
+    delta_lon = lon2Rad - lon1Rad
+
+    a = math.sin(delta_lat/2) ** 2 + math.cos(lat1Rad) * math.cos(lat2Rad) * math.sin(delta_lon/2) ** 2
+    c =  2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    distance = r * c
+    return distance
+
+
+def distance_between(code1, code2, airports):
+    a1 = airports[code1]
+    a2 = airports[code2]
+
+    lat1 = a1["lat"]
+    lon1 = a1["long"]
+    lat2 = a2["lat"]
+    lon2 = a2["long"]
+
+    return harvensine_distance(lat1, lon1, lat2, lon2)
+
+
 
 
 airports = load_airports(airportPath)
@@ -41,3 +71,5 @@ print(airports.get("SGN"))
 
 print(len(graph))
 print(graph.get("SGN"))
+
+print(distance_between("SGN", "SYD", airports))
